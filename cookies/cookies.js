bcffad9d92e7b39e5a311500b0875e03476633e1
Micaela Plata <mmpp4124@gmail.com>
2024-10-22 23:45:30 -0300

    agregando cuadro de cookies

---
+++ cookies/cookies.js
@@ -0,0 +1,17 @@
+
+    document.getElementById('close-icon').addEventListener('click', function() {
+    document.getElementById('aviso-cookies').style.display = 'none'
+    document.getElementById('fondo-cookies').style.display = 'none'
+  });
+
+  document.getElementById('aceptar-cookies').addEventListener('click', function() {
+    document.getElementById('aviso-cookies').style.display = 'none'
+    document.getElementById('fondo-cookies').style.display = 'none'
+    // Codigo de las cookies
+  });
+
+  document.getElementById('rechazar-cookies').addEventListener('click', function() {
+    document.getElementById('aviso-cookies').style.display = 'none'
+    document.getElementById('fondo-cookies').style.display = 'none'
+    // Rechazo de las cookies
+  });
